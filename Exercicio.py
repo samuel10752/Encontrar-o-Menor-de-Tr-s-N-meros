bb03d9ebe92046d0a1f29ea2e5873ea39e5e75d6
samuel10752 <samuelcauabarbosa114@gmail.com>
2024-11-03 12:59:35 -0300

    first commit

---
+++ Exercicio.py
@@ -0,0 +1,8 @@
+def menor_de_tres(x, y, z):
+    return min(x, y, z)
+
+# Exemplo de uso:
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+z = int(input("Digite o terceiro número: "))
+print(f"O menor número é: {menor_de_tres(x, y, z)}")
